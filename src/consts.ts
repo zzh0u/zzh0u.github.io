1da31376c3397639da0627067199013d220bb6e2
zzh0u <weirong.zhou@outlook.com>
2024-11-04 10:33:13 +0800

    configure some params

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = "zzh0u's Blog"
+export const SITE_DESCRIPTION = '请洒潘江，各倾陆海云尔'
